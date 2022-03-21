b9d4402dd8dd7336f8a01895a125fa1c9a1156e0
Ioana-Novacovici <ioananovacovici@gmail.com>
2022-03-21 13:26:35 +0200

    SM-2 Implemented the Student model class

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.studentmanager {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.studentmanager to javafx.fxml;
-    exports com.example.studentmanager;
-}
